2ff5c9330d6cf05fd2900b8889463d5f6cf26230
XieYiZun <Xieyz3@mail2.sysu.edu.cn>
2014-01-21 13:46:55 +0800

    Finish static pages

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -6,6 +6,10 @@ describe "Static pages" do
   	  visit '/static_pages/home'
   	  expect(page).to have_content('Sample App')
   	end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+    end
   end
 
   describe "Help page" do
@@ -13,6 +17,10 @@ describe "Static pages" do
   	  visit '/static_pages/help'
   	  expect(page).to have_content('Help')
   	end
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+    end
   end
 
   describe "About page" do
@@ -20,5 +28,9 @@ describe "Static pages" do
   		visit '/static_pages/about'
   		expect(page).to have_content('About us')
   	end
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')
+    end
   end
 end
