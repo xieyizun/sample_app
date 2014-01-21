d5bb546cc86c39469df269c6235a2dde1d481890
XieYiZun <Xieyz3@mail2.sysu.edu.cn>
2014-01-21 14:42:55 +0800

    Add a contact page

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  let(:base_title) { "Ruby on Rails Tutorial Sample App " }
+
   describe "Home page" do
   	it "should have the content 'Sample App'" do
   	  visit '/static_pages/home'
@@ -8,7 +10,7 @@ describe "Static pages" do
   	end
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+      expect(page).to have_title("#{base_title} | Home ")
     end
   end
 
@@ -19,7 +21,7 @@ describe "Static pages" do
   	end
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+      expect(page).to have_title("#{base_title}| Help")
     end
   end
 
@@ -30,7 +32,19 @@ describe "Static pages" do
   	end
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')
+      expect(page).to have_title("#{base_title}| About ")
     end
   end
+
+  describe "Contact us" do
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title}| Contact ")
+    end
+    it "should have the conten 'Contact us' " do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact us')
+    end
+  end
+
 end
