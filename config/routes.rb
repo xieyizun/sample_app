a53223c6e55d603a459e7db2541f4d3d201d7967
XieYiZun <Xieyz3@mail2.sysu.edu.cn>
2014-02-15 01:50:56 +0800

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SampleAp::Application.routes.draw do
-  get "users/new"
+  resources :users
+  
   root to: "static_pages#home"
   match '/signup', to: "users#new", via: 'get'
   match '/help', to: "static_pages#help", via: 'get'
---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do 
+   factory :user do
+   	name "xieyizun"
+   	email "xieyz3@mail2.sysu.edu.cn"
+   	password "abc111111"
+   	password_confirmation "abc111111"
+   end
+	
+end
