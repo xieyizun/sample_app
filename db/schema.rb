16481af76f6f89911df2f4f6fe094d6d27903790
XieYiZun <Xieyz3@mail2.sysu.edu.cn>
2014-02-15 22:50:23 +0800

    Finsh sign in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212123823) do
+ActiveRecord::Schema.define(version: 20140215114232) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(version: 20140212123823) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "remember_token"
   end
+
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
